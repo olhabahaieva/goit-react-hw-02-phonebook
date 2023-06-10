f021f9ed2836f42b01c0dcc5c587c9e4d6d896aa
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-10 15:21:55 +0300

    added onChange function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
-import Phonebook from "./Phonebook";
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Phonebook/>
-    </div>
-  );
-};
+import { Component } from 'react';
+import Phonebook from './Phonebook';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handlePhonebookClick = inputName =>{
+    this.setState(prevState =>({
+      [inputName]: prevState
+    }))
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <Phonebook onPhonebookClick={this.handlePhonebookClick}/>
+      </div>
+    );
+  }
+}
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import css from './Phonebook.module.css';
 
 class Phonebook extends Component {
-  render(){
-  return (
+  onChange = inputName => {
+    this.props.onChange(inputName);
+  };
+
+  render() {
+    return (
       <div className={css.phonebook}>
         <form className={css.form} action="">
-          <label className={css.label} htmlFor="name">Name</label>
+          <label className={css.label} htmlFor="name">
+            Name
+          </label>
           <input
             type="text"
             name="name"
             pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
+            onChange={this.onChange}
           />
-          <button className={css.button} name='submit' type='submit'>Add contact</button>
+          <button className={css.button} name="submit" type="submit">
+            Add contact
+          </button>
         </form>
       </div>
-
-  );
-};
+    );
+  }
 }
 
 export default Phonebook;

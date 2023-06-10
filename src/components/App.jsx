abc4ebf23f0c65d047d0f174ba8efea8475c7019
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-10 15:45:18 +0300

    fixed styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -20,6 +20,7 @@ export class App extends Component {
         style={{
           height: '100vh',
           display: 'flex',
+          flexDirection: "column",
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,7 +4,7 @@ import css from './Contacts.module.css';
 class Contacts extends Component {
   render() {
     return (
-      <ul>
+      <ul className={css.contacts}>
         <li>{this.props.inputName}</li>
       </ul>
     );

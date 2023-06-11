22178b470025e44cc1a18dfe7ac2963c15e48ade
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-11 20:46:54 +0300

    corrected issue with adding multiple contacts

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import css from './Phonebook.module.css';
 import Section from 'components/Section';
+import { nanoid } from 'nanoid';
 
 class Phonebook extends Component {
-
   state = {
     inputValue: '',
+    contacts: [], // Array to store contacts
   };
 
   onChange = (e) => {
@@ -14,36 +15,49 @@ class Phonebook extends Component {
     });
   };
 
-  //Function for the contacts component to create a new contact based on the button click
   handleButtonClick = (e) => {
     e.preventDefault();
-    this.props.createContact(this.state.inputValue);
+    const { inputValue, contacts } = this.state;
+    const newContact = {
+      id: nanoid(), 
+      name: inputValue,
+    };
+
     this.setState({
+      contacts: [...contacts, newContact], // Add the new contact to the existing contacts array
       inputValue: '', // Reset the input value after creating a contact
     });
-  };  
+  };
 
   render() {
+    const { inputValue, contacts } = this.state;
+
     return (
-      <Section title ='Phonebook'>
-      <div className={css.phonebook}>
-        <form className={css.form} action="">
-          <label className={css.label} htmlFor="name">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.onChange}
-          />
-          <button onClick={this.handleButtonClick} className={css.button} name="submit" type="submit">
-            Add contact
-          </button>
-        </form>
-      </div>
+      <Section title="Phonebook">
+        <div className={css.phonebook}>
+          <form className={css.form} action="">
+            <label className={css.label} htmlFor="name">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Za-яА-Я]+(([' -][a-zA-Za-яА-Я ])?[a-zA-Za-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={inputValue}
+              onChange={this.onChange}
+            />
+            <button onClick={this.handleButtonClick} className={css.button} name="submit" type="submit">
+              Add contact
+            </button>
+          </form>
+        </div>
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
       </Section>
     );
   }

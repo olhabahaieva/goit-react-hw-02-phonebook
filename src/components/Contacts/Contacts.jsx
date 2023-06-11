2bf9638a13a5a65edbe628410a6bcf4538ad7496
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-06-11 20:34:43 +0300

    added css for contacts block

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import css from './Contacts.module.css';
+import Section from 'components/Section';
 
 class Contacts extends Component {
   render() {
@@ -10,9 +11,11 @@ class Contacts extends Component {
     }
 
     return (
+      <Section title='Contacts'>
       <ul className={css.contacts}>
         <li>{inputName}</li>
       </ul>
+      </Section>
     );
   }
 }
